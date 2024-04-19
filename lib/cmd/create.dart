0bf999e89071c222eefcd40849f1995d84830b4e
tomoyuki <tomoyuki@tomoyukinoMacBook-Air.local>
2024-04-19 13:18:56 +0900

    ファイル作成テスト

--- lib/cmd/create.dart
+++ lib/cmd/create.dart
@@ -1,8 +1,31 @@
 import 'base.dart';
+import 'dart:io';
 
 class CreateCmd extends CmdBase {
   @override
   void execute(List<String> args) {
-    print('create ${args.join(' ')}');
+    File('lib/main.dart').writeAsString('''
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text('Welcome to Flutter'),
+        ),
+        body: Center(
+          child: Text('Hello World'),
+        ),
+      ),
+    );
+  }
+}
+''');
   }
 }
