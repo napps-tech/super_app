03ae06e077f636671f4e4ae07f5254393c8df9b9
tomoyuki <tomoyuki@tomoyukinoMacBook-Air.local>
2024-04-19 12:35:23 +0900

    init

---
+++ bin/super_app.dart
@@ -0,0 +1,5 @@
+import 'package:super_app/super_app.dart' as super_app;
+
+void main(List<String> arguments) {
+  print('Hello world: ${super_app.calculate()}!');
+}
---
+++ test/super_app_test.dart
@@ -0,0 +1,8 @@
+import 'package:super_app/super_app.dart';
+import 'package:test/test.dart';
+
+void main() {
+  test('calculate', () {
+    expect(calculate(), 42);
+  });
+}
